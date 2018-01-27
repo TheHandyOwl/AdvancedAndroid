0c564a0850b4c344d672f6647b4763690a10a42a
Carlos <thehandyowl.io@gmail.com>
2018-01-27 23:55:03 +0100

    One line functions

--- domain/src/main/java/com/tho/madridshops/domain/model/Shop.kt
+++ domain/src/main/java/com/tho/madridshops/domain/model/Shop.kt
@@ -16,17 +16,11 @@ data class Shop (val id: Int, val name: String, val address: String) {
  */
 
 class Shops(val shops: MutableList<Shop>): Aggregate<Shop> {
-    override fun count(): Int {
-        return shops.size
-    }
+    override fun count(): Int = shops.size
 
-    override fun all(): List<Shop> {
-        return shops
-    }
+    override fun all(): List<Shop> = shops
 
-    override fun get(position: Int): Shop {
-        return shops.get(position)
-    }
+    override fun get(position: Int): Shop = shops.get(position)
 
     override fun add(element: Shop) {
         shops.add(element)
