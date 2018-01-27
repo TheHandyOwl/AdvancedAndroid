d86550eb0676ffebd7f455660e6ba2c4085c0c93
Carlos <thehandyowl.io@gmail.com>
2018-01-27 19:03:01 +0100

    Getters and setters

--- domain/src/main/java/com/tho/madridshops/domain/model/ShopJava.java
+++ domain/src/main/java/com/tho/madridshops/domain/model/ShopJava.java
@@ -1,8 +1,31 @@
 package com.tho.madridshops.domain.model;
 
-/**
- * Created by carlos on 27/1/18.
- */
-
 public class ShopJava {
+    private int id;
+    private String name;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
