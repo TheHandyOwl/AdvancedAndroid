daae68b9bfe8e48272f8fc0f7bbc42de90450681
Carlos <thehandyowl.io@gmail.com>
2018-01-31 20:56:30 +0100

    Refactor: moving dao test from MadridShopsApp

--- repository/src/main/java/com/tho/madridshops/repository/network/GetJsonManagerVolleyImpl.kt
+++ repository/src/main/java/com/tho/madridshops/repository/network/GetJsonManagerVolleyImpl.kt
@@ -1,13 +1,40 @@
 package com.tho.madridshops.repository.network
 
+import android.content.Context
+import android.util.Log
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
 class GetJsonManagerVolleyImpl: GetJsonManager {
+
+    var context: Context? = null
+    var requestQueue: RequestQueue? = null
+
     override fun execute(url: String) {
         // get request queue
+        // see fun bellow
 
         // create request (success, failure)
+        val request = StringRequest(url,
+                Response.Listener {
+                    Log.d("JSON", it)
+                }, Response.ErrorListener {
+            it.localizedMessage
+        })
 
         // add request to queue
-
+        requestQueue().add(request)
     }
+
+    fun requestQueue(): RequestQueue {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context)
+        }
+
+        return requestQueue !!
+    }
+
 
 }
