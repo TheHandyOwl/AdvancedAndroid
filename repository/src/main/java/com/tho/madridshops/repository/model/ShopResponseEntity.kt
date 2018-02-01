ed933247c02c54b50b37af254442748e4422d274
Carlos <thehandyowl.io@gmail.com>
2018-02-01 19:25:34 +0100

    Test: parsing several shops. New ShopResponseEntity class to parsing shops

---
+++ repository/src/main/java/com/tho/madridshops/repository/model/ShopResponseEntity.kt
@@ -0,0 +1,9 @@
+package com.tho.madridshops.repository.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+internal class ShopResponseEntity (
+    val result: List<ShopEntity>
+)
