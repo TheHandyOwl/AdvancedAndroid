f999fc6f24db23c15921634bb8c40e7cf134f8fd
Carlos <thehandyowl.io@gmail.com>
2018-02-05 01:34:50 +0100

    GetAllShops and DeleteAllShops implemented. Shop list is downloaded and parsed to a DB. Some tests out of order

--- repository/src/main/java/com/tho/madridshops/repository/Repository.kt
+++ repository/src/main/java/com/tho/madridshops/repository/Repository.kt
@@ -1,6 +1,10 @@
 package com.tho.madridshops.repository
 
+import com.tho.madridshops.repository.model.ShopEntity
+
 interface Repository {
+    fun getAllShops(success: (shops: List<ShopEntity>) -> Unit,
+                    error: (errorMessage: String) -> Unit)
     fun deleteAllShops(success: () -> Unit,
                        error: (errorMessage: String) -> Unit)
 }
